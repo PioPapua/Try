e7aa2633fe0b358c1288f4d3cd2ba06bca460ffc
PioPapua <flopy_1991@hotmail.com>
2020-06-29 21:42:21 -0300

    Enable table load and float button
    Table automatically loads every ingredient that is stored in the Room DB.
    
    Floating navigation buttons have been added to every view, replacing static ones.

--- app/src/main/java/com/example/atry/ingredientsTable/IngredientsTable.kt
+++ app/src/main/java/com/example/atry/ingredientsTable/IngredientsTable.kt
@@ -1,11 +1,15 @@
 package com.example.atry.ingredientsTable
 
+import android.app.ActionBar
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -13,6 +17,7 @@ import androidx.navigation.findNavController
 import com.example.atry.R
 import com.example.atry.database.ConzoomDatabase
 import com.example.atry.databinding.FragmentIngredientsTableBinding
+import kotlinx.android.synthetic.main.fragment_ingredients_table.*
 
 class IngredientsTable : Fragment() {
     private lateinit var viewModel: IngredientsTableViewModel
@@ -49,8 +54,45 @@ class IngredientsTable : Fragment() {
         })
 
         viewModel.ingredients.observe(this, Observer { ingredients ->
-            Log.d ("TAG: ", "Ingredientes desde el observer: $ingredients")
+            val rowParams = TableRow.LayoutParams()
+            var textParams = text_id.layoutParams
+            Log.d("TAG: ", "Parametros de fila: $rowParams")
             // TODO Build the table with ingredients
+            for ((index, item) in ingredients.withIndex()) {
+
+                val ingredientRow = TableRow(requireContext())
+
+                val id = TextView(requireContext())
+                id.text = ingredients.elementAt(index).id.toString()
+
+                val name = TextView(requireContext())
+                name.text = ingredients.elementAt(index).name
+
+                val description = TextView(requireContext())
+                description.text = ingredients.elementAt(index).description
+                if (description.text == "null") {description.text = "  -  "}
+
+                val warning = TextView(requireContext())
+                if (ingredients.elementAt(index).warning) {
+                    warning.text = context?.getString(R.string.yes)
+                } else {
+                    warning.text = context?.getString(R.string.no)
+                }
+
+                val categoryType = TextView(requireContext())
+                categoryType.text = ingredients.elementAt(index).categoryType
+
+                ingredientRow.addView(id)
+                ingredientRow.addView(name)
+                ingredientRow.addView(description)
+                ingredientRow.addView(warning)
+                ingredientRow.addView(categoryType)
+
+                ingredientRow.layoutParams = rowParams
+                ingredientRow.background = context?.getDrawable(R.drawable.table_border)
+
+                tableHeader.addView(ingredientRow)
+            }
         })
 
         return binding.root
